dddad456afc1bf8f7f8dff8b498ccc8d3a0e3b20
atreavez <alvin69david@gmail.com>
2026-05-01 21:36:21 +0300

    Writing first Java File

---
+++ Assignments/cit-java-week3/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, CiT! Welcome to Java.");
+        System.out.println(10 + 10);
+        System.out.println("I am Alvin" + " David");
+        String greetings = "Hello, World!";
+        System.out.println(greetings);
+        int length = greetings.length();
+        System.out.println(length);
+        String UpperCase = greetings.toUpperCase();
+        System.out.println(UpperCase);
+        var Name = "Drena Jennifer";
+        System.out.println(Name);
+        var Age = 20;
+        System.out.println(Age);
+    }
+}
