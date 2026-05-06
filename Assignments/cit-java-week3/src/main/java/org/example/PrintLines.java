e50f0ca6c322a27170a1c8d5ce9cf041985c9efb
atreavez <alvin69david@gmail.com>
2026-05-06 19:20:34 +0300

    Worked in Print Numbers

--- Assignments/cit-java-week3/src/main/java/org/example/PrintLines.java
+++ Assignments/cit-java-week3/src/main/java/org/example/PrintLines.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class PrintLines {
     public static void main(String[] args) {
-        System.out.println(3+3);
-        System.out.println(2*5);
-        System.out.println(3-6);
-        System.out.println(6/3);
+        System.out.println(3+3); //Addition
+        System.out.println(2*5); //Multiplication
+        System.out.println(3-6); //Subtraction
+        System.out.println(6/3); //Division
         System.out.print(4);
     }
 }
