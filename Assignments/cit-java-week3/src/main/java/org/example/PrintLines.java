c2ff75b2e1dee4b9ce753e5d4320927d17698b6f
atreavez <alvin69david@gmail.com>
2026-05-06 15:29:56 +0300

    Worked in Print Numbers

---
+++ Assignments/cit-java-week3/src/main/java/org/example/PrintLines.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class PrintLines {
+    public static void main(String[] args) {
+        System.out.println(3+3);
+        System.out.println(2*5);
+        System.out.println(3-6);
+        System.out.println(6/3);
+        System.out.print(4);
+    }
+}
