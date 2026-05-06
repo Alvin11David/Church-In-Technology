9ab19386803007fb5171401cef56c992c9bef0f5
atreavez <alvin69david@gmail.com>
2026-05-06 19:30:39 +0300

    Worked on variables

---
+++ Assignments/cit-java-week3/src/main/java/org/example/Variables.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/*
+Java has the following data types {String, int, char, boolean, float, double*/
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Waluube Alvin David";
+        int age = 22;
+        double amount = 120000000.89;
+        float cgpa = 4.9f;
+        char gender = 'M';
+        boolean isMale = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(amount);
+        System.out.println(cgpa);
+        System.out.println(gender);
+        System.out.println(isMale);
+    }
+}
