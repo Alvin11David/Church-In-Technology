8699d9c3c78607fc7fb489a8ea877039f73a089a
atreavez <alvin69david@gmail.com>
2026-05-06 19:23:11 +0300

    Worked on comments both single line and multiline

--- Assignments/cit-java-week3/src/main/java/org/example/PrintLines.java
+++ Assignments/cit-java-week3/src/main/java/org/example/PrintLines.java
@@ -7,5 +7,7 @@ public class PrintLines {
         System.out.println(3-6); //Subtraction
         System.out.println(6/3); //Division
         System.out.print(4);
+        /*
+         This is a multi line comment for longer comments*/
     }
 }
